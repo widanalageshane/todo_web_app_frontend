048de14da9905e8f4266ecf3fdd692b50cf841a1
Shane Widanalage <t3wish00@students.oamk.fi>
2024-03-23 18:48:56 +0200

    frontend first commit

---
+++ js/class/Todos.js
@@ -0,0 +1,82 @@
+import { Task } from "./Task.js"
+
+class Todos {
+    #tasks = []
+    #backend_url = ''
+
+    constructor (url) {
+        this.#tasks = [];
+        this.#backend_url = url;
+
+
+    }
+
+    getTasks =() => {   
+        return new Promise( async(resolve, reject) => {
+            fetch(this.#backend_url)
+            .then((response) => response.json())    
+            .then((json) => {
+                this.#readJson(json);
+                resolve(this.#tasks);
+            }, (error) => {
+                reject(error);
+            });
+        });
+    }
+
+    removeTask = (id) => {
+        return new Promise( async(resolve, reject) => {
+            fetch(this.#backend_url + '/delete/' + id, {
+                method: 'delete'
+            })
+            .then((response) => response.json())
+            .then((json) => {
+                this.#removeFromArray(id);
+                resolve(json.id);
+            }, (error) => {
+                reject(error);
+            });
+        });
+    }
+ 
+    #readJson = (taskAsJson) => {    
+        taskAsJson.forEach(node => {
+            const task =new Task(node.id, node.description)
+            this.#tasks.push(task);
+        });
+    
+    }
+    
+
+    addTask =(text) => {
+        return new Promise( async(resolve, reject) => {
+            const json = JSON.stringify({description: text});
+            fetch(this.#backend_url + '/new', {
+                method: 'post',
+                headers: {'Content-Type': 'application/json'},
+                body: json
+             })
+            .then((response) => response.json())
+            .then((json) => {
+                resolve(this.#addToArray(json.id, text));
+            },(error) => {
+                reject(error);
+            });
+        });
+    }
+
+   
+    #addToArray = (id, text) => {
+        const task = new Task(id, text);
+        this.#tasks.push(task);
+        return task;
+    }
+
+    #removeFromArray = (id) => {
+        const arrayWithoutRemoved = this.#tasks.filter(task => task.id !== id);
+        this.#tasks = arrayWithoutRemoved;
+    }
+
+}
+
+export { Todos }
